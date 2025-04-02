e7e2863580e0f81c650c4fd5ef815d288a08105a
hetalhalani <hetal.halani@sjsu.edu>
2025-04-02 10:02:15 -0700

    meal logs page

--- FoodDetection/Views/Profile.swift
+++ FoodDetection/Views/Profile.swift
@@ -9,6 +9,24 @@ struct Profile: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 25) {
+                Image(systemName: "person.circle.fill") //replace with such that user can add photo?
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.white)
+                    .padding(.top, 50)
+                
+                
+                Text("Email: ")
+                Text("Name: ")
+                Text("Age: ")
+                Text("Height: ")
+                Text("Weight: ")
+                Text("Goals: ")
+                Text("Notifications") //add a box to tick or not
+                Text("Change Password")
+            }
         }
     }
 }
